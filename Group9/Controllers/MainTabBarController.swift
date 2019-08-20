7e5f1714683e5e63b5544c739ab5021aad176977
ridolud <faridho.lutfi@gmail.com>
2019-08-20 19:19:28 +0700

    First init

--- Group9/Controllers/MainTabBarController.swift
+++ Group9/Controllers/MainTabBarController.swift
@@ -8,39 +8,11 @@
 
 import UIKit
 
-class MainTabBarController: UITabBarController, UITabBarControllerDelegate {
+class MainTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        delegate = self
+        
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        let item1 = NearByController()
-        let tabItem1 = UITabBarItem(title: "Near By", image: nil, tag: 1)
-        item1.tabBarItem = tabItem1
-        
-        let item2 = SearchController()
-        let tabItem2 = UITabBarItem(title: "Search", image: nil, tag: 1)
-        item2.tabBarItem = tabItem2
-        
-        let item3 = ProfileController()
-        let tabItem3 = UITabBarItem(title: "Profile", image: nil, tag: 1)
-        item3.tabBarItem = tabItem3
-        
-        self.viewControllers = [item1, item2, item3]
-    }
-    
-    /*
-    // Delegate
-    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
-        print("Should select viewController: \(viewController.title ?? "") ?")
-        return true;
-    }
-    */
-    
-    
 
 }
