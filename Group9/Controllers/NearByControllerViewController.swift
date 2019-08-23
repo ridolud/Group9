cf0958650c162a42a70e73773139b31fc14d6858
Azis Jabbar Susetio <tioazis@gmail.com>
2019-08-23 16:59:34 +0700

    largeTitleBar

--- Group9/Controllers/NearByControllerViewController.swift
+++ Group9/Controllers/NearByControllerViewController.swift
@@ -14,7 +14,8 @@ class NearByControllerViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationController?.navigationBar.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         
         let wrapper = StoreCollectionView.instance
         wrapper.categoryName = "Bulk Store"
---
+++ Group9/Models/Article.swift
@@ -0,0 +1,10 @@
+
+//
+//  article.swift
+//  Group9
+//
+//  Created by Azis Jabbar Susetio on 23/08/19.
+//  Copyright © 2019 ridolud. All rights reserved.
+//
+
+import Foundation
